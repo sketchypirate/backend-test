c9c0e8564e005fc010632e3cd3afe3afc2643308
toranderr <56118225+toranderr@users.noreply.github.com>
2022-12-05 18:58:53 +0100

    Update views.py

--- the_project/quickstart/views.py
+++ the_project/quickstart/views.py
@@ -1,31 +1,17 @@
 # see https://www.django-rest-framework.org/ for more info
 # some example/pseudo is written below
 
-from rest_framework.views import APIView#, Response, status
+from rest_framework.views import APIView
 
 class ExampleView(APIView):
 
     def post(self, request, *args, **kwargs):
-        #body = request.data
-        #return Response({"random_data":1}, status=status.HTTP_204_NO_CONTENT)
-
         raise NotImplementedError()
 
     def get(self, request, *args, **kwargs):
-        # query_parameters = request.query_parameters
-
-        # serializer = ExampleSerializer(instance=instance_you_retrieved, context={})
-        # data = serializer.data
-
         raise NotImplementedError()
 
     def patch(self, request, *args, **kwargs):
-
-        # serializer = ExampleSerializer(instance=instance_you_retrieved, data=data_you_retrieved, context={})
-        # serializer.is_valid(raise_exception=True)
-        # instance = serializer.save()
-        # data = serializer.data
-
         raise NotImplementedError()
 
     def delete(self, request, *args, **kwargs):
